8475085f433ac69f48482062fe48b963b276498f
raj2710prince <125546050+raj2710prince@users.noreply.github.com>
2023-08-05 21:01:57 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+let songs = [
+    {
+        name: 'meri jaan',
+        path: ' musics/song1.mp3',
+        artist: 'King',
+    },
+    {
+        name: 'unstoppable',
+        path: ' musics/song2.mp3',
+        artist: 'Sia',
+    }
+    
+]
+
