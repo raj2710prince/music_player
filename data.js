0c7b6978ae4f7722cdbdfb01be78e512bcc8b931
raj2710prince <125546050+raj2710prince@users.noreply.github.com>
2023-08-05 21:02:44 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,12 +1,12 @@
 let songs = [
     {
         name: 'meri jaan',
-        path: ' musics/song1.mp3',
+        path: 'song1.mp3',
         artist: 'King',
     },
     {
         name: 'unstoppable',
-        path: ' musics/song2.mp3',
+        path: 'song2.mp3',
         artist: 'Sia',
     }
     
